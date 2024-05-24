b6ee2fd80f2c2b7d75995e123f737aa3490c3061
Yevhenii Sarancha <yevhenii.sarancha@outlook.com>
2024-05-24 23:33:35 +0300

    implemented most of the needed logics

---
+++ src/swagger.js
@@ -0,0 +1,39 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Auth API",
+      version: "1.0.0",
+      description: "A simple Express Auth API",
+    },
+    servers: [
+      {
+        url: "http://localhost:5000",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["./routes/*.js"], // Path to the API docs
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = (app) => {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+};
